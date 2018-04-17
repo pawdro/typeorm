c0d3313d801beb6371c36c70072c46225cbbb427
Paweł Drożański <pawel.drozanski@netguru.pl>
2018-04-17 23:54:14 +0200

    simple DB operations

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,22 @@ import {Photo} from "./entity/Photo";
 
 createConnection().then(async connection => {
 
-    let photo = new Photo();
-    photo.name = "Me and Bears";
-    photo.description = "I am near polar bears";
-    photo.filename = "photo-with-bears.jpg";
-    photo.views = 1;
-    photo.isPublished = true;
-
-    // await connection.manager.save(photo);
-    // console.log("Photo has been saved with id: ", photo.id);
-
     let photoRepository = connection.getRepository(Photo);
 
-    await photoRepository.save(photo);
-    console.log("Photo has been saved");
+    let firstPhoto = await photoRepository.findOneById(1);
+    console.log("First photo from the db: ", firstPhoto);
 
-    let savedPhotos = await photoRepository.find();
-    console.log("All photos from the db: ", savedPhotos);
+    let meAndBearsPhoto = await photoRepository.findOne({ name: "Me and Bears" });
+    console.log("Me and Bears photo from the db: ", meAndBearsPhoto);
+
+    let allViewedPhotos = await photoRepository.find({ views: 1 });
+    console.log("All viewed photos: ", allViewedPhotos);
+
+    let allPublishedPhotos = await photoRepository.find({ isPublished: true });
+    console.log("All published photos: ", allPublishedPhotos);
+
+    let [allPhotos, photosCount] = await photoRepository.findAndCount();
+    console.log("All photos: ", allPhotos);
+    console.log("Photos count: ", photosCount);
 
 }).catch(error => console.log(error));
