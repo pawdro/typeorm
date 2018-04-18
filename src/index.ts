e0a0675fee66063bc9ab3c5c4e71fbdad2af3d07
Paweł Drożański <pawel.drozanski@netguru.pl>
2018-04-18 09:55:59 +0200

    photo_metadata relation

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,38 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {Photo} from "./entity/Photo";
+import {PhotoMetadata} from "./entity/PhotoMetadata";
 
-createConnection().then(async connection => {
+createConnection(/*...*/).then(async connection => {
 
+    // create a photo
+    let photo = new Photo();
+    photo.name = "Me and Bears";
+    photo.description = "I am near polar bears";
+    photo.filename = "photo-with-bears.jpg";
+    photo.isPublished = true;
+    photo.views = 1;
+
+    // create a photo metadata
+    let metadata = new PhotoMetadata();
+    metadata.height = 640;
+    metadata.width = 480;
+    metadata.compressed = true;
+    metadata.comment = "cybershoot";
+    metadata.orientation = "portait";
+    metadata.photo = photo; // this way we connect them
+
+    // get entity repositories
     let photoRepository = connection.getRepository(Photo);
+    let metadataRepository = connection.getRepository(PhotoMetadata);
 
-    let firstPhoto = await photoRepository.findOneById(1);
-    console.log("First photo from the db: ", firstPhoto);
+    // first we should save a photo
+    await photoRepository.save(photo);
 
-    let meAndBearsPhoto = await photoRepository.findOne({ name: "Me and Bears" });
-    console.log("Me and Bears photo from the db: ", meAndBearsPhoto);
+    // photo is saved. Now we need to save a photo metadata
+    await metadataRepository.save(metadata);
 
-    let allViewedPhotos = await photoRepository.find({ views: 1 });
-    console.log("All viewed photos: ", allViewedPhotos);
-
-    let allPublishedPhotos = await photoRepository.find({ isPublished: true });
-    console.log("All published photos: ", allPublishedPhotos);
-
-    let [allPhotos, photosCount] = await photoRepository.findAndCount();
-    console.log("All photos: ", allPhotos);
-    console.log("Photos count: ", photosCount);
+    // done
+    console.log("Metadata is saved, and relation between metadata and photo is created in the database too");
 
 }).catch(error => console.log(error));
